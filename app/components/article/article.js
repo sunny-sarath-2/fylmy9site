9505207607525079349e1102d9d4d41479f8fcae
sunny-sarath-2 <sunnsarath2@gmail.com>
2019-10-01 21:47:19 +0530

    minor changes

--- app/components/article/article.js
+++ app/components/article/article.js
@@ -6,12 +6,17 @@ import RelatedArticles from "./relatedArticles";
 import Tags from "./tags";
 
 const articke = props => {
-  console.log(props);
-  const { title, review_created_date, review, movie_tags } = props.news;
+  const { title, image, review_created_date, review, movie_tags } = props.news;
+  console.log(image);
   return (
     <div className="col-12 col-md-7 col-lg-8">
       <article>
         <Header title={title} date={review_created_date} />
+        {image ? (
+          <section class="article-pic">
+            <img src={image} />
+          </section>
+        ) : null}
         <Content review={review} />
         <FooterSocial />
         <RelatedArticles />
