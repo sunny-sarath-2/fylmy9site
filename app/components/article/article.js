79f8455b9c1dd97a0366fd2c001d629b936483d1
sunny-sarath-2 <sunnsarath2@gmail.com>
2019-10-04 18:26:54 +0530

    article ready

--- app/components/article/article.js
+++ app/components/article/article.js
@@ -6,7 +6,15 @@ import RelatedArticles from "./relatedArticles";
 import Tags from "./tags";
 
 const articke = props => {
-  const { title, image, review_created_date, review, movie_tags } = props.news;
+  const {
+    title,
+    image,
+    review_created_date,
+    review,
+    movie_tags,
+    tags,
+    article
+  } = props.news;
   return (
     <div className="col-12 col-md-7 col-lg-8">
       <article>
@@ -16,10 +24,10 @@ const articke = props => {
             <img src={image} />
           </section>
         ) : null}
-        <Content review={review} />
+        <Content review={review ? review : article} />
         <FooterSocial />
         <RelatedArticles />
-        <Tags tags={movie_tags} />
+        <Tags tags={movie_tags ? movie_tags : tags} />
       </article>
     </div>
   );
